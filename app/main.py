3bd0715d67863e59d97941f3faf95b2236cd4894
GabbyWhitis <gabbylonza@gmail.com>
2023-05-01 14:49:12 -0400

    simplifying flask for testing

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import jwt
+import os
 
 app = Flask(__name__)
 
@@ -14,38 +14,5 @@ PACKAGES = [
 def authenticate():
     return jsonify({'message': 'This system does not support authentication.'}), 501
 
-@app.route('/packages', methods=['POST'])
-def packages_list():
-    # Parse request body
-    package_queries = request.json
-
-    for query in package_queries:
-        if 'Name' not in query:
-            return jsonify({'error': "There is missing field(s) in the PackageQuery/AuthenticationToken\
-            \ or it is formed improperly, or the AuthenticationToken is invalid."}), 400
-    
-    # Check for pagination offset
-    offset = request.args.get('offset', 0)
-
-    print("offset ", offset)
-        
-    # Mock database query
-    results = []
-
-    for package in PACKAGES:
-        for query in package_queries:
-            print(query)
-            if query == '*' or query == package['Name']:
-                results.append(package)
-    
-    # Apply pagination
-    paginated_results = results[int(offset):int(offset)+10]  # limit to 10 results per page
-    
-    # Generate response
-    response = jsonify(paginated_results)
-    response.headers.add('offset', str(int(offset)+10))  # set next page offset in response header
-    
-    return response, 200
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
