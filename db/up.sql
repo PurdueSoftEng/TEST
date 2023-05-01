2dc490387003cc7b9c8622a110f99ba7cd7d05cb
GabbyWhitis <gabbylonza@gmail.com>
2023-05-01 00:30:26 -0400

    creating workflow to deploy to gcp

---
+++ db/up.sql
@@ -0,0 +1,96 @@
+CREATE TABLE packages (
+  package_id SERIAL,
+  url VARCHAR,
+  version VARCHAR NOT NULL,
+  package_name VARCHAR NOT NULL,
+  jsprogram VARCHAR,
+  content VARCHAR,
+  metric_one REAL,
+  metric_two REAL,
+  metric_three REAL,
+  metric_four REAL,
+  metric_five REAL,
+  metric_six REAL,
+  metric_seven REAL,
+  total_score REAL,
+  PRIMARY KEY(package_id)
+);
+
+-- CREATE TABLE users (
+--   user_id SERIAL,
+--   user_name VARCHAR NOT NULL,
+--   isAdmin BOOLEAN NOT NULL,
+--   PRIMARY KEY(user_id)
+-- );
+
+CREATE TABLE users (
+  username VARCHAR(50) NOT NULL,
+  isAdmin BOOLEAN NOT NULL,
+  token VARCHAR(255),
+  PRIMARY KEY(username)
+);
+
+CREATE TABLE requests (
+  request_id SERIAL,
+  request_type VARCHAR NOT NULL,
+  PRIMARY KEY(request_id)
+);
+
+CREATE TABLE groups (
+  group_id SERIAL,
+  group_name VARCHAR NOT NULL,
+  PRIMARY KEY(group_id)
+);
+
+CREATE TABLE history_log (
+  history_id SERIAL,
+  time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  group_id INT NOT NULL,
+  package_id INT NOT NULL,
+  request_id INT NOT NULL,
+  PRIMARY KEY(history_id)
+);
+
+CREATE TABLE registry (
+  registry_id SERIAL,
+  group_id INT NOT NULL,
+  package_id INT NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY(registry_id)
+);
+
+ALTER TABLE history_log
+    ADD CONSTRAINT history_log_group_id 
+    FOREIGN KEY (group_id) 
+    REFERENCES groups(group_id) 
+    ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE history_log
+    ADD CONSTRAINT history_log_package_id 
+    FOREIGN KEY (package_id) 
+    REFERENCES packages(package_id) 
+    ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE history_log
+    ADD CONSTRAINT history_log_request_id 
+    FOREIGN KEY (request_id) 
+    REFERENCES requests(request_id) 
+    ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE registry
+    ADD CONSTRAINT registry_group_id 
+    FOREIGN KEY (group_id) 
+    REFERENCES groups(group_id) 
+    ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE registry
+    ADD CONSTRAINT registry_package_id 
+    FOREIGN KEY (package_id) 
+    REFERENCES packages(package_id) 
+    ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE registry
+    ADD CONSTRAINT registry_user_id 
+    FOREIGN KEY (user_id) 
+    REFERENCES users(user_id) 
+    ON UPDATE CASCADE ON DELETE RESTRICT;
--- index.py
+++ index.py
@@ -10,53 +10,32 @@ PACKAGES = [
     {"Version": "^1.2.3", "Name": "React", "ID": "react"},
 ]
 
-# Define a dictionary of authorized users and their passwords
-authorized_users = {
-    "alice": "password123",
-    "bob": "password456"
-}
-
-# Define an endpoint to handle authentication requests
-@app.route('/authenticate', methods=['POST'])
+@app.route('/authenticate', methods=['PUT'])
 def authenticate():
-    # Parse the authentication request body
-    req_data = request.get_json()
-    username = req_data['User']['name']
-    password = req_data['Secret']['password']
-
-    # Check if the provided credentials are valid
-    if username in authorized_users and authorized_users[username] == password:
-        # Return a JWT token as the authentication token
-        auth_token = generate_jwt_token(username)
-        return jsonify(auth_token), 200
-    else:
-        return "Invalid username or password", 401
-
-# Define a function to generate a JWT token for the authenticated user
-def generate_jwt_token(username):
-    # Use a library like PyJWT to generate a JWT token
-    jwt_secret = 'my_secret_key'
-    token = jwt.encode({'username': username}, jwt_secret, algorithm='HS256')
-    return token
+    return jsonify({'message': 'This system does not support authentication.'}), 501
 
 @app.route('/packages', methods=['POST'])
 def packages_list():
     # Parse request body
     package_queries = request.json
-    
-    # Check for authentication token
-    auth_token = request.headers.get('X-Authorization')
-    if not auth_token:
-        return jsonify({"error": "Authentication token missing"}), 400
+
+    for query in package_queries:
+        if 'Name' not in query:
+            return jsonify({'error': "There is missing field(s) in the PackageQuery/AuthenticationToken\
+            \ or it is formed improperly, or the AuthenticationToken is invalid."}), 400
     
     # Check for pagination offset
     offset = request.args.get('offset', 0)
-    
+
+    print("offset ", offset)
+        
     # Mock database query
     results = []
+
     for package in PACKAGES:
         for query in package_queries:
-            if query['Name'] == '*' or query == package:
+            print(query)
+            if query == '*' or query == package['Name']:
                 results.append(package)
     
     # Apply pagination
