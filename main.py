d56d16a2aec9e0480302744fd2c7a66dbd5f7169
GabbyWhitis <gabbylonza@gmail.com>
2023-05-01 14:54:51 -0400

    using the example api

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from flask import Flask, request, jsonify
 import os
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
-# Mock data for packages
-PACKAGES = [
-    {"Version": "1.2.3", "Name": "Underscore", "ID": "underscore"},
-    {"Version": "1.2.3-2.1.0", "Name": "Lodash", "ID": "lodash"},
-    {"Version": "^1.2.3", "Name": "React", "ID": "react1"},
-]
 
-@app.route('/authenticate', methods=['PUT'])
-def authenticate():
-    return jsonify({'message': 'This system does not support authentication.'}), 501
+@app.route('/')
+def hello_world():
+    name = request.args.get('name', 'World')
+    return f'Hello {name}!'
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
