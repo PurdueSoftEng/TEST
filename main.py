6c0dd5827fb3c72c05de12e0e0cc8c9bbe190fa7
GabbyWhitis <gabbylonza@gmail.com>
2023-05-01 22:19:54 -0400

    testing db connection

--- main.py
+++ main.py
@@ -13,24 +13,24 @@ PACKAGES = [
     {"Version": "^1.2.3", "Name": "React", "ID": "react1"},
 ]
 
-# Set up database connection details
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASSWORD')
-db_name = os.environ.get('DB_NAME')
-db_host = os.environ.get('DB_HOST')
-db_port = os.environ.get('DB_PORT')
+# # Set up database connection details
+# db_user = os.environ.get('DB_USER')
+# db_password = os.environ.get('DB_PASSWORD')
+# db_name = os.environ.get('DB_NAME')
+# db_host = os.environ.get('DB_HOST')
+# db_port = os.environ.get('DB_PORT')
 
-# Create PyMySQL connection
-conn = pymysql.connect(
-    user=db_user,
-    password=db_password,
-    host=db_host,
-    port=int(db_port),
-    database=db_name,
-)
+# # Create PyMySQL connection
+# conn = pymysql.connect(
+#     user=db_user,
+#     password=db_password,
+#     host=db_host,
+#     port=int(db_port),
+#     database=db_name,
+# )
 
-# Create SQLAlchemy engine
-engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}', echo=True)
+# # Create SQLAlchemy engine
+# engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}', echo=True)
 
 # Define table metadata
 metadata = MetaData()
@@ -40,18 +40,18 @@ test_table = Table('test_table', metadata,
                    Column('value', Float),
                   )
 
-# Create the table in the database
-metadata.create_all(engine)
+# # Create the table in the database
+# metadata.create_all(engine)
 
-@app.route('/add-table', methods=['POST'])
-def add_table():
-    # Insert new record into the test_table
-    query = test_table.insert().values(name='test', value=1.23)
-    with conn.cursor() as cursor:
-        cursor.execute(str(query))
-        conn.commit()
+# @app.route('/add-table', methods=['POST'])
+# def add_table():
+#     # Insert new record into the test_table
+#     query = test_table.insert().values(name='test', value=1.23)
+#     with conn.cursor() as cursor:
+#         cursor.execute(str(query))
+#         conn.commit()
 
-    return jsonify({'message': 'Table added successfully!'})
+#     return jsonify({'message': 'Table added successfully!'})
 
 @app.route('/')
 def hello_world():
