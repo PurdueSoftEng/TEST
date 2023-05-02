4d090ccf3767cc95f528a6453a6ef787957a1637
GabbyWhitis <gabbylonza@gmail.com>
2023-05-01 21:27:13 -0400

    changed db credentials

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+
+import pymysql
 import os
 from flask import Flask, request, jsonify
 import mysql.connector
 import requests
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, Float,LargeBinary, pool, text, exc, MetaData
 
 app = Flask(__name__)
 
@@ -12,53 +17,37 @@ PACKAGES = [
     {"Version": "^1.2.3", "Name": "React", "ID": "react1"},
 ]
 
-# Get database connection parameters from environment variables
-# db_host = os.environ.get('DB_HOST')
-# db_user = os.environ.get('root')
-# db_password = os.environ.get('')
-# db_name = os.environ.get('gabby-sql')
-db_host = '35.184.227.219'
-db_user = 'root'
-db_password = ''
-db_name = 'gabby-sql'
+def getconn() -> pymysql.connections.Connection:
+    conn: pymysql.connections.Connection = Connector().connect(
+        "purdue-soft-eng-384818:us-central1:gabby-sql",
+        "pymysql",
+        user="root",
+        password="",
+        db="gabby-sql"
+    )
+    return conn
 
-# Configure database connection
-db = mysql.connector.connect(
-    host=db_host,
-    user=db_user,
-    password=db_password,
-    database=db_name
-)
+def create_engine_with_conn_pool() -> sqlalchemy.engine.Engine:
+    engine = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+        poolclass=pool.QueuePool,
+        pool_pre_ping=True)
+    return engine   
 
-# Get database connection parameters from environment variables
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASSWORD')
-db_name = os.environ.get('DB_NAME')
-
-# Get Cloud SQL instance hostname from the metadata server
-metadata_server_url = 'http://metadata.google.internal/computeMetadata/v1/instance/attributes/'
-instance_hostname = requests.get(metadata_server_url + 'hostname', headers={'Metadata-Flavor': 'Google'}).text
-
-# Construct database connection parameters
-db_config = {
-    'user': db_user,
-    'password': db_password,
-    'host': instance_hostname,
-    'database': db_name,
-    'unix_socket': '/cloudsql/{}'.format(instance_hostname)
-}
-
-# Establish database connection
-db = mysql.connector.connect(**db_config)
+engine = create_engine_with_conn_pool()
+metadata = MetaData()
 
 @app.route('/')
 def hello_world():
     name = request.args.get('name', 'World')
     return f'Howdy {name}!'
 
-@app.route('/checkdbcredentials')
-def check_db_credentials():
-    return f'Dbhost: {db_host}\nDbuser: {db_user}\nDbpassword: {db_password}\nDbdb: {db_name}!'
+@app.route('/createtable')
+def create_table():
+    with engine.connect() as conn:
+        metadata.create_all(conn)
+    return jsonify({'message': 'Table created successfully!'})
 
 @app.route('/authenticate', methods=['PUT'])
 def authenticate():
