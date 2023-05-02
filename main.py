ced7608db6eb22e6e333ee4d8ca587362917b8ec
GabbyWhitis <gabbylonza@gmail.com>
2023-05-02 00:33:10 -0400

    working on adding a package endpoint

--- main.py
+++ main.py
@@ -71,38 +71,38 @@ def hello_world():
 def authenticate():
     return jsonify({'message': 'This system does not support authentication.'}), 501
 
-@app.route('/package', methods=['POST'])
-def packages_list():
-    # Parse request body
-    package_queries = request.json
+# @app.route('/package', methods=['POST'])
+# def packages_list():
+#     # Parse request body
+#     package_queries = request.json
 
-    for query in package_queries:
-        if 'Name' not in query:
-            return jsonify({'error': "There is missing field(s) in the PackageQuery/AuthenticationToken\
-            \ or it is formed improperly, or the AuthenticationToken is invalid."}), 400
+#     for query in package_queries:
+#         if 'Name' not in query:
+#             return jsonify({'error': "There is missing field(s) in the PackageQuery/AuthenticationToken\
+#             \ or it is formed improperly, or the AuthenticationToken is invalid."}), 400
     
-    # Check for pagination offset
-    offset = request.args.get('offset', 0)
+#     # Check for pagination offset
+#     offset = request.args.get('offset', 0)
 
-    print("offset ", offset)
+#     print("offset ", offset)
         
-    # Mock database query
-    results = []
+#     # Mock database query
+#     results = []
 
-    for package in PACKAGES:
-        for query in package_queries:
-            print(query)
-            if query == '*' or query == package['Name']:
-                results.append(package)
+#     for package in PACKAGES:
+#         for query in package_queries:
+#             print(query)
+#             if query == '*' or query == package['Name']:
+#                 results.append(package)
     
-    # Apply pagination
-    paginated_results = results[int(offset):int(offset)+10]  # limit to 10 results per page
+#     # Apply pagination
+#     paginated_results = results[int(offset):int(offset)+10]  # limit to 10 results per page
     
-    # Generate response
-    response = jsonify(paginated_results)
-    response.headers.add('offset', str(int(offset)+10))  # set next page offset in response header
+#     # Generate response
+#     response = jsonify(paginated_results)
+#     response.headers.add('offset', str(int(offset)+10))  # set next page offset in response header
     
-    return response, 200
+#     return response, 200
 
 
 @app.route('/package', methods=['POST'])
@@ -120,8 +120,6 @@ def add_package():
     response = request_body
 
     return response, 201
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
