0c12e485bf388c46e5f9c1163236fc0cefb3c6e0
GabbyWhitis <gabbylonza@gmail.com>
2023-05-01 22:14:04 -0400

    testing db connection

--- main.py
+++ main.py
@@ -2,11 +2,7 @@
 import pymysql
 import os
 from flask import Flask, request, jsonify
-import mysql.connector
-import requests
-from google.cloud.sql.connector import Connector
-import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String, Float,LargeBinary, pool, text, exc, MetaData
+from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, Table
 
 app = Flask(__name__)
 
@@ -17,37 +13,50 @@ PACKAGES = [
     {"Version": "^1.2.3", "Name": "React", "ID": "react1"},
 ]
 
-def getconn() -> pymysql.connections.Connection:
-    conn: pymysql.connections.Connection = Connector().connect(
-        "purdue-soft-eng-384818:us-central1:gabby-sql",
-        "pymysql",
-        user="root",
-        password="",
-        db="gabby-sql"
-    )
-    return conn
+# Set up database connection details
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
 
-def create_engine_with_conn_pool() -> sqlalchemy.engine.Engine:
-    engine = sqlalchemy.create_engine(
-        "mysql+pymysql://",
-        creator=getconn,
-        poolclass=pool.QueuePool,
-        pool_pre_ping=True)
-    return engine   
+# Create PyMySQL connection
+conn = pymysql.connect(
+    user=db_user,
+    password=db_password,
+    host=db_host,
+    port=int(db_port),
+    database=db_name,
+)
 
-engine = create_engine_with_conn_pool()
+# Create SQLAlchemy engine
+engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}', echo=True)
+
+# Define table metadata
 metadata = MetaData()
+test_table = Table('test_table', metadata,
+                   Column('id', Integer, primary_key=True),
+                   Column('name', String(255)),
+                   Column('value', Float),
+                  )
+
+# Create the table in the database
+metadata.create_all(engine)
+
+@app.route('/add-table', methods=['POST'])
+def add_table():
+    # Insert new record into the test_table
+    query = test_table.insert().values(name='test', value=1.23)
+    with conn.cursor() as cursor:
+        cursor.execute(str(query))
+        conn.commit()
+
+    return jsonify({'message': 'Table added successfully!'})
 
 @app.route('/')
 def hello_world():
     name = request.args.get('name', 'World')
     return f'Howdy {name}!'
-
-@app.route('/createtable')
-def create_table():
-    with engine.connect() as conn:
-        metadata.create_all(conn)
-    return jsonify({'message': 'Table created successfully!'})
 
 @app.route('/authenticate', methods=['PUT'])
 def authenticate():
