9b04fea60c06f71e92a6058b1e2f93915b1188e0
GabbyWhitis <gabbylonza@gmail.com>
2023-05-01 23:23:22 -0400

    simplifying api again for testing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-
-import pymysql
-import os
 from flask import Flask, request, jsonify
-from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, Table
+import os
 
 app = Flask(__name__)
 
@@ -10,29 +7,8 @@ app = Flask(__name__)
 PACKAGES = [
     {"Version": "1.2.3", "Name": "Underscore", "ID": "underscore"},
     {"Version": "1.2.3-2.1.0", "Name": "Lodash", "ID": "lodash"},
-    {"Version": "^1.2.3", "Name": "React", "ID": "react"},
+    {"Version": "^1.2.3", "Name": "React", "ID": "react1"},
 ]
-
-# Define table metadata
-metadata = MetaData()
-test_table = Table('test_table', metadata,
-                   Column('id', Integer, primary_key=True),
-                   Column('name', String(255)),
-                   Column('value', Float),
-                  )
-
-# # Create the table in the database
-# metadata.create_all(engine)
-
-# @app.route('/add-table', methods=['POST'])
-# def add_table():
-#     # Insert new record into the test_table
-#     query = test_table.insert().values(name='test', value=1.23)
-#     with conn.cursor() as cursor:
-#         cursor.execute(str(query))
-#         conn.commit()
-
-#     return jsonify({'message': 'Table added successfully!'})
 
 @app.route('/')
 def hello_world():
@@ -43,39 +19,5 @@ def hello_world():
 def authenticate():
     return jsonify({'message': 'This system does not support authentication.'}), 501
 
-@app.route('/packages', methods=['POST'])
-def packages_list():
-    # Parse request body
-    package_queries = request.json
-
-    for query in package_queries:
-        if 'Name' not in query:
-            return jsonify({'error': "There is missing field(s) in the PackageQuery/AuthenticationToken\
-            \ or it is formed improperly, or the AuthenticationToken is invalid."}), 400
-    
-    # Check for pagination offset
-    offset = request.args.get('offset', 0)
-
-    print("offset ", offset)
-        
-    # Mock database query
-    results = []
-
-    for package in PACKAGES:
-        for query in package_queries:
-            print(query)
-            if query == '*' or query == package['Name']:
-                results.append(package)
-    
-    # Apply pagination
-    paginated_results = results[int(offset):int(offset)+10]  # limit to 10 results per page
-    
-    # Generate response
-    response = jsonify(paginated_results)
-    response.headers.add('offset', str(int(offset)+10))  # set next page offset in response header
-    
-    return response, 200
-
-
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
