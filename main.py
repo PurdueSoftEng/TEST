e76f5d4241d2039bb153bc8c94f84103a6b0f7d1
GabbyWhitis <gabbylonza@gmail.com>
2023-05-01 23:52:11 -0400

    still trying to connect to db

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ db_user = os.environ.get('root')
 db_password = os.environ.get('')
 db_name = os.environ.get('gabby-sql')
 db_host = os.environ.get('35.192.15.50')
-db_port = os.environ.get(3306)
+db_port = os.environ.get('3306')
 
 # Create PyMySQL connection
 conn = pymysql.connect(
